a17e255017485c6c8198d75c220b028b84d7e1f6
YathinKumar11 <95606896+YathinKumar11@users.noreply.github.com>
2022-02-14 01:04:20 +0530

    Update main.dart

--- campus_automation/lib/main.dart
+++ campus_automation/lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'building.dart';
 import 'form.dart';
 import 'login_page.dart';
 import 'home_page.dart';
-
+import 'active_requests.dart';
+import 'form2.dart';
 void main() => runApp(MyApp());
+
+
+class User{
+  late String rollnumber;
+  late String name;
+  late String roomnumber;
+  User(this.rollnumber,this.name, this.roomnumber);
+}
 
 class MyApp extends StatelessWidget {
   final routes = <String, WidgetBuilder>{
     LoginPage.tag: (context) => const LoginPage(),
     HomePage.tag: (context) => HomePage(),
     SignUpPage.tag: (context) => SignUpPage(),
+    Active_Requests.tag:(context) => Active_Requests(),
+    laundry.tag:(context) => laundry(),
+    Building.tag: (context) => Building(),
   };
 
    MyApp({Key? key}) : super(key: key);
